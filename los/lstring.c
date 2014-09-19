9b697498293ca8cf1fdde7f0a183838c64ec2024
deglingo <ahmidmefes@gmail.com>
2014-09-19 16:53:39 +0200

    fixed some memory leaks

--- los/lstring.c
+++ los/lstring.c
@@ -8,12 +8,36 @@
 
 
 
+static void _dispose ( LObject *object );
+
+
+
+/* l_string_class_init:
+ */
+static void l_string_class_init ( LObjectClass *cls )
+{
+  cls->dispose = _dispose;
+}
+
+
+
+/* _dispose:
+ */
+static void _dispose ( LObject *object )
+{
+  g_free(L_STRING(object)->str);
+  L_STRING(object)->str = NULL;
+  L_STRING(object)->len = 0;
+}
+
+
+
 /* l_string_new:
  */
 LString *l_string_new ( const gchar *str )
 {
   LString *lstr = L_STRING(l_object_new(L_CLASS_STRING, NULL));
   lstr->len = strlen(str);
-  lstr->str = strdup(str);
+  lstr->str = g_strdup(str);
   return lstr;
 }
