63d5803b2d0b9f11e5592a077f70c1c6458b5db1
deglingo <ahmidmefes@gmail.com>
2014-09-24 22:36:13 +0200

    implemented LInt.to_string()

--- los/lint.c
+++ los/lint.c
@@ -6,6 +6,19 @@
 
 
 
+static gchar *_to_string ( LObject *object );
+
+
+
+/* l_int_class_init:
+ */
+static void l_int_class_init ( LObjectClass *cls )
+{
+  ((LObjectClass *) cls)->to_string = _to_string;
+}
+
+
+
 /* l_int_new:
  */
 LInt *l_int_new ( gint value )
@@ -13,4 +26,15 @@ LInt *l_int_new ( gint value )
   LInt *i = L_INT(l_object_new(L_CLASS_INT, NULL));
   i->value = value;
   return i;
+}
+
+
+
+/* _to_string:
+ */
+static gchar *_to_string ( LObject *object )
+{
+  return g_strdup_printf("<%s %d>",
+                         l_object_class_name(L_OBJECT_GET_CLASS(object)),
+                         L_INT_VALUE(object));
 }
